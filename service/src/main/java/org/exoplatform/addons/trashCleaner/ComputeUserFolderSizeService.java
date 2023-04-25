56f992829be840ea00186777e3c7140e5eb014f2
Romain Dénarié <romain.denarie@exoplatform.com>
2023-04-25 16:20:12 +0200

    Change feature to compute users folders size Browse jcr folders and check if user need to be computed instead of checking first users

--- service/src/main/java/org/exoplatform/addons/trashCleaner/ComputeUserFolderSizeService.java
+++ service/src/main/java/org/exoplatform/addons/trashCleaner/ComputeUserFolderSizeService.java
@@ -1,18 +1,24 @@
 package org.exoplatform.addons.trashCleaner;
 
-import org.exoplatform.commons.serialization.B;
-import org.exoplatform.commons.utils.ListAccess;
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import javax.annotation.security.RolesAllowed;
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
 import org.exoplatform.container.ExoContainerContext;
 import org.exoplatform.container.component.RequestLifeCycle;
 import org.exoplatform.services.jcr.RepositoryService;
-import org.exoplatform.services.jcr.config.RepositoryConfigurationException;
-import org.exoplatform.services.jcr.core.ManageableRepository;
 import org.exoplatform.services.jcr.ext.app.SessionProviderService;
-import org.exoplatform.services.jcr.ext.common.SessionProvider;
-import org.exoplatform.services.jcr.ext.distribution.DataDistributionManager;
-import org.exoplatform.services.jcr.ext.distribution.DataDistributionMode;
-import org.exoplatform.services.jcr.ext.distribution.DataDistributionType;
-import org.exoplatform.services.jcr.ext.hierarchy.NodeHierarchyCreator;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
 import org.exoplatform.services.organization.OrganizationService;
@@ -20,100 +26,45 @@ import org.exoplatform.services.organization.User;
 import org.exoplatform.services.organization.UserStatus;
 import org.exoplatform.services.rest.resource.ResourceContainer;
 
-import javax.annotation.security.RolesAllowed;
-import javax.jcr.Node;
-import javax.jcr.NodeIterator;
-import javax.jcr.PathNotFoundException;
-import javax.jcr.RepositoryException;
-import javax.jcr.Session;
-import javax.ws.rs.*;
-import javax.ws.rs.core.Response;
 import io.swagger.v3.oas.annotations.Parameter;
-
-import java.text.CharacterIterator;
-import java.text.StringCharacterIterator;
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.util.Arrays;
 
 @Path("computeUserFolderSize")
 public class ComputeUserFolderSizeService implements ResourceContainer {
   private static final Log LOG = ExoLogger.getLogger(ComputeUserFolderSizeService.class);
-  RepositoryService        repositoryService;
-  SessionProviderService sessionProviderService;
-  OrganizationService organizationService;
-  NodeHierarchyCreator nodeHierarchyCreator;
-  DataDistributionManager dataDistributionManager;
 
-  Node userNodes;
-
-  private final int BATCH_SIZE = 500;
-
-  public ComputeUserFolderSizeService(DataDistributionManager dataDistributionManager, NodeHierarchyCreator nodeHierarchyCreator,OrganizationService organizationService,RepositoryService repositoryService, SessionProviderService sessionProviderService) {
+  private SessionProviderService sessionProviderService;
+  private RepositoryService repositoryService;
+  private OrganizationService organizationService;
+  int totalUsersCount;
+  public ComputeUserFolderSizeService(SessionProviderService sessionProviderService, RepositoryService repositoryService, OrganizationService organizationService) {
+    this.sessionProviderService=sessionProviderService;
     this.repositoryService = repositoryService;
-    this.organizationService = organizationService;
-    this.nodeHierarchyCreator = nodeHierarchyCreator;
-    this.sessionProviderService = sessionProviderService;
-    this.dataDistributionManager = dataDistributionManager;
+    this.organizationService=organizationService;
   }
 
   @GET
   @RolesAllowed("administrators")
   public Response computeUserFolderSizeSize(@Parameter(description = "Check for user not connected since this date (format timestamp in ms)") @QueryParam("date") String date) {
-    var ref = new Object() {
-      Instant limitDate = Instant.now();
-    };
+    Instant limitDate = Instant.now();
     if (date == null || date.equals("")) {
-      ref.limitDate = ref.limitDate.minus(365*2, ChronoUnit.DAYS);
+      limitDate = limitDate.minus(365*2, ChronoUnit.DAYS);
     } else {
-      ref.limitDate = Instant.ofEpochMilli(Long.parseLong(date));
+      limitDate = Instant.ofEpochMilli(Long.parseLong(date));
     }
-    LOG.info("Compute Users Folder size for user not connected since {}", ref.limitDate);
+    LOG.info("Compute Users Folder size for user not connected since {}", limitDate);
     int totalSize = 0;
-    long startTime = System.currentTimeMillis();
-
+    this.totalUsersCount = 0;
     try {
-      ListAccess<User> users = organizationService.getUserHandler().findAllUsers(UserStatus.ANY);
-      int current = 0;
-      int total = users.getSize();
-      User[] usersArray;
-      do {
-        RequestLifeCycle.begin(ExoContainerContext.getCurrentContainer());
 
-        long startTimeLoadUsers = System.currentTimeMillis();
-        usersArray = users.load(current, Math.min(total - current, BATCH_SIZE));
-        LOG.debug("Load {} users in {} ms", Math.min(total - current, BATCH_SIZE), System.currentTimeMillis() - startTimeLoadUsers);
+      long startTime = System.currentTimeMillis();
 
-        long startTimeTreatment = System.currentTimeMillis();
-        totalSize += Arrays.stream(usersArray).filter(user -> {
-          if (isConnected(user)) {
-            Instant lastLoginTime = user.getLastLoginTime().toInstant();
-            if (lastLoginTime.isBefore(ref.limitDate)) {
-              LOG.debug("User {} lastLoginTime ({}) is before limitDate ({}), need to compute size", user.getUserName(), user.getLastLoginTime(), ref.limitDate);
-              return true;
-            }
-          } else {
-            Instant createdDate = user.getCreatedDate().toInstant();
-            if (createdDate.isBefore(ref.limitDate)) {
-              LOG.debug("User {} never connected and created ({}) before limitDate ({}) , need to compute size", user.getUserName(), user.getCreatedDate(), ref.limitDate);
-              return true;
-            }
-          }
-          return false;
-        }).reduce(0, (subtotal, user) -> {
-          //compute user folder size
-          int userFolderSize = computeUserFolderSize(user.getUserName());
-          return subtotal+userFolderSize;
-        }, Integer::sum);
+      Session session = sessionProviderService.getSystemSessionProvider(null).getSession("collaboration",repositoryService.getDefaultRepository());
 
-        LOG.info("Treat {} users ({}/{}) in {} ms", Math.min(total - current, BATCH_SIZE),current+usersArray.length,total, System.currentTimeMillis() - startTimeLoadUsers);
+      Node userRootNode = session.getRootNode().getNode("Users");
 
-        current+=usersArray.length;
-        RequestLifeCycle.end();
+      totalSize = browserUsersFolders(userRootNode,limitDate);
 
-      } while (usersArray.length == BATCH_SIZE);
-
-      String result = "Total size for user not connected since "+ref.limitDate.toString()+" is "+humanReadableByteCountBin(totalSize)+", execution time "+(System.currentTimeMillis() - startTime)+" ms";
+      String result = "Total size for users not connected since "+limitDate.toString()+" is "+humanReadableByteCountBin(totalSize)+", for a total of "+totalUsersCount+" users, execution time "+(System.currentTimeMillis() - startTime)+" ms";
 
       LOG.info(result);
       return Response.ok(result).build();
@@ -124,29 +75,67 @@ public class ComputeUserFolderSizeService implements ResourceContainer {
     }
   }
 
-  private int computeUserFolderSize(String userName) {
+  private int browserUsersFolders(Node currentNode, Instant limitDate) {
+    NodeIterator iterator = null;
+    String path=null;
+    int subTotalSize = 0;
     try {
-      Node userNode = getUserNode(this.sessionProviderService.getSystemSessionProvider(null), userName);
-      return computeSubFolderSize(userNode);
-    } catch (PathNotFoundException pe) {
-      //user folder not exists
-      return 0;
-    } catch (Exception e) {
-      LOG.error("Unable to compute user node size for user {}", userName,e);
-      return 0;
+      path=currentNode.getPath();
+      iterator = currentNode.getNodes();
+    } catch (RepositoryException e) {
+      LOG.error("Error when reading child nodes of {}",path,e);
+      return subTotalSize;
     }
+    long startTime = System.currentTimeMillis();
+
+    while (iterator.hasNext()) {
+      Node child = iterator.nextNode();
+      String username ="";
+      try {
+        username = child.getName();
+        if (child.isNodeType("exo:userFolder")) {
+          //check if user is connected
+          User user = getUser(username);
+          if (isConnected(user)) {
+            Instant lastLoginTime = user.getLastLoginTime().toInstant();
+            if (lastLoginTime.isBefore(limitDate)) {
+              LOG.debug("User {} lastLoginTime ({}) is before limitDate ({}), need to compute size", user.getUserName(), user.getLastLoginTime(), limitDate);
+              subTotalSize += computeSubFolderSize(child);
+              this.totalUsersCount++;
+            }
+          } else {
+            Instant createdDate = user.getCreatedDate().toInstant();
+            if (createdDate.isBefore(limitDate)) {
+              LOG.debug("User {} never connected and created ({}) before limitDate ({}) , need to compute size", user.getUserName(), user.getCreatedDate(), limitDate);
+              subTotalSize += computeSubFolderSize(child);
+              this.totalUsersCount++;
+            }
+          }
+          if (this.totalUsersCount % 3 ==0) {
+            LOG.info("Progression : {} users folder computed",this.totalUsersCount);
+          }
+        } else {
+          subTotalSize += browserUsersFolders(child, limitDate);
+        }
+      } catch (RepositoryException e) {
+        LOG.error("Error when computing user folder size for user {}",username);
+      }
+    }
+
+    LOG.debug("Spent {} ms to browse folder {}",System.currentTimeMillis() - startTime, path);
+    return subTotalSize;
   }
 
-  private Node getUserNode(SessionProvider systemSessionProvider, String userName) throws
-                                                                                   RepositoryException,
-                                                                                   RepositoryConfigurationException {
-
-    if (this.userNodes==null) {
-      Session session = systemSessionProvider.getSession("collaboration", (ManageableRepository) repositoryService.getDefaultRepository());
-      this.userNodes = (Node)session.getItem("/Users");
+  private User getUser(String username) {
+    RequestLifeCycle.begin(ExoContainerContext.getCurrentContainer());
+    try {
+      return organizationService.getUserHandler().findUserByName(username,UserStatus.ANY);
+    } catch (Exception e) {
+      LOG.error("Unable to find user {}",username);
+      return null;
+    } finally {
+      RequestLifeCycle.end();
     }
-    DataDistributionType dataDistributionType = dataDistributionManager.getDataDistributionType(DataDistributionMode.READABLE);
-    return dataDistributionType.getDataNode(this.userNodes,userName);
   }
 
   private boolean isConnected(User user) {
